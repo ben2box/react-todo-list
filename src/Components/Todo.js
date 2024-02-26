20dc50c977ef0f5bb827d5efb10f72abe2164b84
ben2box <pedrobento92@gmail.com>
2024-02-26 13:19:02 +0000

    improved task list css

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,4 +1,11 @@
+import { useState } from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {faTrash} from '@fortawesome/free-solid-svg-icons'
+
 export default function Todo({ todo, handleUpdateTodo, handleRemoveTodo }){
+
+    const removeIcon = <FontAwesomeIcon icon={faTrash} />
+
   
     const handleCompleteToggle = () => handleUpdateTodo({
       ...todo,
@@ -8,10 +15,15 @@ export default function Todo({ todo, handleUpdateTodo, handleRemoveTodo }){
     const handleDeleteClick = () => handleRemoveTodo(todo.id)
   
     return (
-      <li>
+      <li className={todo.completed === true ? 'task-complete' : 'task-incomplete'}>
         {todo.label}
-        <button onClick={handleCompleteToggle}>{todo.completed ? 'Complete' : 'Incomplete'}</button>
-        <button onClick={handleDeleteClick}>Delete</button>
+        <button 
+          className={todo.completed === true ? 'btn btn-success btn-sm' : 'btn btn-warning btn-sm'}
+          onClick={handleCompleteToggle}>
+          {todo.completed ? 'Complete' : 'Incomplete'}
+        </button>
+
+        <button className='btn btn-danger btn-sm' onClick={handleDeleteClick}>{removeIcon}</button>
       </li>
     )
   }
